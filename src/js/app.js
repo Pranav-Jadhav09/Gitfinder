d18497fb37cb9e79a10a5dc05c677d90b20d95f3
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-18 13:56:16 +0530

    feat: 🚀 search bar open on click

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,45 @@
+"use strict";
+
 /**
- * @license MIT
- * @author jrpranav <pranav@jrpranav.com>
- * @copyright jrpranav 2024
+ * Add eventlistner on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType String
+ * @param {Function} callback Function
  */
 
-"use strict";
+const addEventOnElements = function ($elements, eventType, callback) {
+  for (const $item of $elements) {
+    $item.addEventListener(eventType, callback);
+  }
+};
+
+/**
+ * Header Scroll State
+ */
+
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Search Toggle
+ */
+
+const /** {NodeElemet} */ $searchToggler = document.querySelector(
+    "[data-search-toggler]"
+  );
+const /** {NodeElement} */ $searchField = document.querySelector(
+    "[data-search-field]"
+  );
+let /** {Boolean} */ isExpanded = false;
+
+$searchToggler.addEventListener("click", function () {
+  $header.classList.toggle("search-active");
+
+  isExpanded = isExpanded ? false : true;
+  this.setAttribute("aria-expanded", isExpanded);
+
+  $searchField.focus();
+});
