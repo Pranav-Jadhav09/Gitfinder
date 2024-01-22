8ac2848ba68afe2ec498a991d32c2a952a6e56ad
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-22 12:28:20 +0530

    feat: 🚨 Pubic API Integrated

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,20 @@
+"use strict";
+
 /**
- * @license MIT
- * @author jrpranav <pranav@jrpranav.com>
- * @copyright jrpranav 2024
+ * Fetch data from server
+ * @param {*} url API URL [required]
+ * @param {*} seccessCalback Success callback [required]
+ * @param {*} errorCallback Error callback [optional]
  */
 
-"use strict";
+export async function fetchData(url, successCallback, errorCallback) {
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  } else {
+    const error = await response.json();
+    errorCallback && errorCallback(error);
+  }
+}
